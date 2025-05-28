2cae9d59e750d3269522e6b542cb4b401ed72899
bryan_alvarado18 <117047716+Bryan18-Alvarado@users.noreply.github.com>
2025-05-27 19:06:17 -0600

    inicializando configuracion docente

---
+++ src/interfaces/docente.interface.ts
@@ -0,0 +1,47 @@
+export interface Docente {
+  id: number;
+  nombre: string;
+  apellido: string;
+  edad: string;
+  genero_id: number;
+  genero?: {
+    id: number;
+    nombre: string;
+  };
+  estado_civil_id: number;
+  estado_civil?: {
+    id: number;
+    nombre: string;
+  };
+  codigo_laboral: string;
+  cursos_asignados: string;
+  direccion?: string;
+  fecha_ingreso: string;
+  fecha_nacimiento: string;
+  telefono: string;
+  email?: string;
+  isAvailable: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+  deletedAt?: string;
+}
+
+export interface DocenteData {
+  nombre: string;
+  apellido: string;
+  edad: string;
+  genero_id: number;
+  estado_civil_id: number;
+  codigo_laboral: string;
+  cursos_asignados: string;
+  direccion?: string;
+  fecha_ingreso: string;
+  fecha_nacimiento: string;
+  telefono: string;
+  email?: string;
+}
+
+export interface DocentesResponse {
+  data: Docente[];
+  total: number;
+}
